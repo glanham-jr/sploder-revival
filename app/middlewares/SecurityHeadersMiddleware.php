49b96af249a08f0dc166571e2fa7c5153e90eb54
Grant Lanham <contact@grantlanham.com>
2025-11-30 20:15:55 -0500

    disable content security policy pre-migration

--- app/middlewares/SecurityHeadersMiddleware.php
+++ app/middlewares/SecurityHeadersMiddleware.php
@@ -19,6 +19,7 @@ class SecurityHeadersMiddleware
 	{
 		$nonce = $this->app->get('csp_nonce');
 
+        /*
 		// development mode to execute Tracy debug bar CSS
 		$tracyCssBypass = "'nonce-{$nonce}'";
 		if(Debugger::$showBar === true) {
@@ -26,8 +27,11 @@ class SecurityHeadersMiddleware
 		}
 
 		$csp = "default-src 'self'; script-src 'self' 'nonce-{$nonce}' 'strict-dynamic'; style-src 'self' {$tracyCssBypass}; img-src 'self' data:;";
+         */
+
 		$this->app->response()->header('X-Frame-Options', 'SAMEORIGIN');
-		$this->app->response()->header("Content-Security-Policy", $csp);
+        // TODO: re-enable Content-Security-Policy
+		// $this->app->response()->header("Content-Security-Policy", $csp);
 		$this->app->response()->header('X-XSS-Protection', '1; mode=block');
 		$this->app->response()->header('X-Content-Type-Options', 'nosniff');
 		$this->app->response()->header('Referrer-Policy', 'no-referrer-when-downgrade');
